5085ac0c1ab40108538ceb2f2300316bb14255dd
Athirat Kasornkaew018 <apexgaming.th@gmail.com>
2025-12-14 11:31:09 +0700

    login+regis

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -10,6 +10,7 @@
     <div class=" flex flex-1 w-full  ">
       <div class="w-full flex flex-col justify-center items-center">
         <h1 class=" text-2xl font-semibold ">ลงชื่อเข้าใช้บัญชี</h1>
+        <form @submit.prevent="handlelogin()">
         <div class=" flex flex-col mt-15 space-y-4 justify-center items-center ">
           <div>
             <h1>ชื่อผู้ใช้งาน</h1>
@@ -29,8 +30,51 @@
           </div>
 
         </div>
+        </form>
       </div>
     </div>
 
   </div>
 </template>
+<script setup>
+import { ref } from 'vue';
+import axios from 'axios';
+import {jwtDecode} from 'jwt-decode';
+import { useRouter } from 'vue-router';
+
+const router = useRouter()
+const keyuser =ref('')
+const password = ref('')
+
+const handlelogin = async () => {
+  try {
+    const response = await axios.post(`${import.meta.env.VITE_API}/api/login`,
+      {
+        keyuser: keyuser.value,
+        password: password.value
+      }
+    )
+      const token = response.data.token
+      localStorage.setItem('authtoken', token)
+
+      const decoded = jwtDecode(token)
+      
+      if(decoded.role === 'user'){
+        alert('เข้าสํู่ระบบสำเร็จ')
+        router.push('/user')
+      } else if(decoded.role === 'boss') {
+         alert('เข้าสํู่ระบบสำเร็จ')
+        router.push('/boss')
+      } else if(decoded.role === 'officer') {
+         alert('เข้าสํู่ระบบสำเร็จ')
+        router.push('/officer')
+      }
+
+      
+  } catch(err) {
+    console.error(err)
+    alert('ฉีผิด')
+  }
+}
+  
+</script>
